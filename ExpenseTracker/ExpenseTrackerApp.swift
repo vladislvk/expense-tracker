7f18dfca50e50224d7e1ae410646071a7bcdefad
vladislvk <vladislavkim566@gmail.com>
2024-10-03 23:13:38 +0500

    feat:blank

--- ExpenseTracker/ExpenseTrackerApp.swift
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -1,10 +1,20 @@
 import SwiftUI
+import SwiftData
+
 
 @main
 struct ExpenseTrackerApp: App {
+    
+    let container: ModelContainer = {
+        let schema = Schema([Expense.self])
+        let container = try! ModelContainer(for: schema, configurations: [])
+        return container
+    }()
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(container)
+        .modelContainer(for:[Expense.self])
     }
 }
